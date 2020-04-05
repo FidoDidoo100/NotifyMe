f76fc8fdc9915f325445b13c03f9378b64e57e89
Iavor Orlyov <iavor_orlyov@yahoo.com>
2020-04-05 19:11:40 +0300

    Added Courses Page with state

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import CoursePage from "./CoursesPage";
 import Layout from "./common/Layout";
 
 function App() {
   function getPage() {
     const route = window.location.pathname;
+    if (route === "/courses") return <CoursePage />;
     if (route === "/about") return <AboutPage />;
 
     return <HomePage />;
